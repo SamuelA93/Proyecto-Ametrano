c6783755fa3c212c6f6218013ea5db9271c6a394
SamuelAstol <samuel.astol@gmail.com>
2014-07-28 22:16:08 -0300

    comentado de clase

--- MTS/src/Grafica/Controladores/Controlador_Tabla_Tareas_Particulares.java
+++ MTS/src/Grafica/Controladores/Controlador_Tabla_Tareas_Particulares.java
@@ -9,9 +9,9 @@ import Logica_Persistencia.Value_Object.VOTareaParticular;
 
 public class Controlador_Tabla_Tareas_Particulares {
 			
-	public Controlador_Tabla_Tareas_Particulares(){}
+	/*public Controlador_Tabla_Tareas_Particulares(){}
 	public List<VOTareaParticular> listarTareaParticular(){
 		Fachada f = new Fachada();
 		return f.listarTareaParticular();
-	}
+	}*/
 }
